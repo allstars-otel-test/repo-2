c3d8cd7f79260acce2ff631a9b677848bc83d457
sakshi-1505 <sakshipatle.0808cs171134@gmail.com>
2024-01-02 01:13:03 +0530

    use a web server
    Signed-off-by: sakshi-1505 <sakshipatle.0808cs171134@gmail.com>

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world from repo-2")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	addr := ":8080"
+
+	log.Printf("Server starting on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
